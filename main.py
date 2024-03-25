0502daeeeddc7b0f837e19129cf3f9b1b4c91f6d
Denys Chernyshov <den.blackshov@gmail.com>
2024-03-25 15:58:30 +0100

    Fixing modules stuff

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ parent_folder_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(parent_folder_path)
 sys.path.append(os.path.join(parent_folder_path, 'lib'))
 sys.path.append(os.path.join(parent_folder_path, 'plugin'))
+sys.path.append(os.path.join(parent_folder_path, 'lib/lxml'))
 
-import lxml
+from lxml import etree
 import requests
 from bs4 import BeautifulSoup
 import reverso_api
@@ -16,7 +17,13 @@ from flowlauncher import FlowLauncher
 import webbrowser
 from collections import defaultdict
 
-help('modules')
+with open("modules", "a") as f:
+    f.writelines("\nRUNNING\n")
+    f.writelines(",".join(sys.modules.keys()))
+    f.writelines(["\n"])
+
+# source = BeautifulSoup(requests.get("https://google.com").text, features="lxml")
+# help('modules')
 
 class ReversoFlow(FlowLauncher):
     path = "Images/app.png"
