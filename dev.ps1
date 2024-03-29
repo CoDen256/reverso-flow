bb5f734aacc007daa960535fd9c4959c1d60a7eb
Denys Chernyshov <den.blackshov@gmail.com>
2024-03-29 12:51:18 +0100

    Trying to fix build

--- dev.ps1
+++ dev.ps1
@@ -1,8 +1,9 @@
+taskkill /f /im Flow*
+
 Set-Variable -Name "FLOW_HOME" (Get-ChildItem -Dir -Path "~\scoop\apps\flow-launcher\current" -Filter 'app*').FullName
-Remove-Item -Path "$FLOW_HOME\UserData\Plugins\Reverso-Flow-*" -Recurse -Force
+Remove-Item -Path "$FLOW_HOME\UserData\Plugins\Reverso-Flow-*" -Force -Recurse
 New-Item -Path "$FLOW_HOME\UserData\Plugins\Reverso-Flow-Dev" -ItemType Junction -Value $PWD\..\reverso-flow -Force
 
 python311.exe -m pip install -r "./requirements.txt" -t "./lib"
 
-taskkill /f /im Flow*
 Start-Process -FilePath "$FLOW_HOME\Flow.Launcher.exe"
