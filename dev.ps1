022ea76ab8f5fcab2b39fad49f50da4af10f8523
Denys Chernyshov <den.blackshov@gmail.com>
2024-03-29 13:36:21 +0100

    Clean up

--- dev.ps1
+++ dev.ps1
@@ -4,6 +4,7 @@ Set-Variable -Name "FLOW_HOME" (Get-ChildItem -Dir -Path "~\scoop\apps\flow-laun
 Remove-Item -Path "$FLOW_HOME\UserData\Plugins\Reverso-Flow-*" -Force -Recurse
 New-Item -Path "$FLOW_HOME\UserData\Plugins\Reverso-Flow-Dev" -ItemType Junction -Value $PWD\..\reverso-flow -Force
 
+python311.exe -m pip install --upgrade pip
 python311.exe -m pip install -r "./requirements.txt" -t "./lib"
 
 Start-Process -FilePath "$FLOW_HOME\Flow.Launcher.exe"
--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
 import itertools
-import pprint
+import os
 import sys
 import time
-import os
 
 parent_folder_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(parent_folder_path)
 sys.path.append(os.path.join(parent_folder_path, 'lib'))
 sys.path.append(os.path.join(parent_folder_path, 'plugin'))
-sys.path.append(os.path.join(parent_folder_path, 'lib/lxml'))
 
-import requests
 import ctypes
 import re
 import reverso_api
